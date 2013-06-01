1722bb6b4bad51185a73dfbe2bd492f1c2dbf21d
kostas <lamprou@live.com>
2013-06-01 21:24:03 +0300

    more methods added

--- index.js
+++ index.js
@@ -31,3 +31,61 @@ instagram.media = function(media_id, callback){
    		}
    	});
 }
+
+instagram.feed = function(callback){
+	jsonp("https://api.instagram.com/v1/users/self/feed?access_token="+ settings.access_token +"", {}, function(err, data){	
+   		if (callback){
+   			callback(err, data);
+   		}
+   	});
+}
+
+instagram.me = function(user_id, callback){
+	jsonp("https://api.instagram.com/v1/users/"+ user_id +"/media/recent/?access_token="+ settings.access_token +"", {}, function(err, data){	
+   		if (callback){
+   			callback(err, data);
+   		}
+   	});
+}
+
+instagram.popular = function(callback){
+	jsonp("https://api.instagram.com/v1/media/popular?access_token="+ settings.access_token +"", {}, function(err, data){	
+   		if (callback){
+   			callback(err, data);
+   		}
+   	});
+}
+
+instagram.tag = function(tag_name, callback){
+	jsonp("https://api.instagram.com/v1/tags/"+ tag_name +"/media/recent?access_token="+ settings.access_token +"", {}, function(err, data){	
+   		if (callback){
+   			callback(err, data);
+   		}
+   	});
+}
+
+instagram.follows = function(user_id, callback){
+	jsonp("https://api.instagram.com/v1/users/"+ user_id +"/follows?access_token="+ settings.access_token +"", {}, function(err, data){	
+   		if (callback){
+   			callback(err, data);
+   		}
+   	});
+}
+
+instagram.followers = function(user_id, callback){
+	jsonp("https://api.instagram.com/v1/users/"+ user_id +"/followed-by?access_token="+ settings.access_token +"", {}, function(err, data){	
+   		if (callback){
+   			callback(err, data);
+   		}
+   	});
+}
+
+instagram.nearby = function(lat, lng, callback){
+	jsonp("https://api.instagram.com/v1/media/search?lat="+lat+"&lng="+lng+"&?access_token="+ settings.access_token +"", {}, function(err, data){	
+   		if (callback){
+   			callback(err, data);
+   		}
+   	});
+}
+
+
