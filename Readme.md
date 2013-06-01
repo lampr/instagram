e35034d19a74945633bfcb0155b7f20f378d4383
kostas <lamprou@live.com>
2013-06-01 16:02:31 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+
+# Instagram
+
+  A simple Instagram API communication.
+
+## Installation
+
+    $ component install lampr/Instagram
+
+## API
+
+   
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Module dependencies
+ */
+
+var jsonp = require('jsonp');
+
+/**
+ * Main Function
+ */
+ 
+ 
+function instagram (access_token, scope, callback) {  
+   	jsonp("https://api.instagram.com/v1/media/"+ scope +"?access_token="+ access_token +"", {}, function(err, data){	
+   		if (callback){
+   			callback(err, data);
+   		}
+   	});
+}
