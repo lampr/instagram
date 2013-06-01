95c29af00169641bad244804df65e6ded6874063
kostas <lamprou@live.com>
2013-06-01 20:57:16 +0300

    progress

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ var jsonp = require('jsonp');
 module.exports = instagram;
 
 /**
- * Main Function
+ * Main Functions
  */
  
- 
-function instagram (access_token, scope, callback) {  
-   	jsonp("https://api.instagram.com/v1/media/"+ scope +"?access_token="+ access_token +"", {}, function(err, data){	
+
+var settings = {
+	access_token: function(token){
+		return token;
+	}, 
+	client_id: function(client_id){
+		return client_id;
+	}
+}
+
+function instagram (token, client_id) {
+	settings.access_token = token
+}
+
+instagram.media = function(media_id, callback){
+	jsonp("https://api.instagram.com/v1/media/"+ media_id +"?access_token="+ settings.access_token +"", {}, function(err, data){	
    		if (callback){
    			callback(err, data);
    		}
